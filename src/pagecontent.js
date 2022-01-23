177d4f7f1e38445211bc10b5c135034710f7a0da
BananaBJones <95904947+CodingFunky@users.noreply.github.com>
2022-01-23 11:12:10 -0700

    ,m

--- src/pagecontent.js
+++ src/pagecontent.js
@@ -1,1 +1,15 @@
-contentContainer = document.querySelector('#content');
+// const contentContainer = document.getElementById('content')
+// const links = document.querySelector('.links')
+
+// console.log(contentContainer)
+// console.log(links)
+
+// menuContent = ['Burger: $8.99', "Fries: $2.99", 'Drink: $0.99'];
+
+// let menu = document.createElement('div');
+// menu.classList.add('menu');
+// menu.textContent = menuContent;
+// // contentContainer.appendChild(menu);
+
+
+
